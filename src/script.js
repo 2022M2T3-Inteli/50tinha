ae64cd5e89d3e98b9b21a91b056659ef4f5310cb
Israel <israel.carvalho@sou.inteli.edu.br>
2022-05-18 16:58:55 -0300

    Aprimorei a tela de login

---
+++ src/script.js
@@ -0,0 +1,17 @@
+//Login
+var visibilityClick = false
+function frg(){
+    const eye = document.getElementById("eye")
+    const input = document.getElementById("keyInput")
+    if(visibilityClick == false){
+        eye.innerHTML = "visibility_off"
+        input.type = "text"
+        visibilityClick = true
+    }
+    else{
+        eye.innerHTML = "visibility"
+        visibilityClick = false
+        input.type = "password"
+        console.log("invisible")
+    }
+}
