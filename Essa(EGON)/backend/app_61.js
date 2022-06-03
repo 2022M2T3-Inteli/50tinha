c47a0bef22ca8cf6b8f371548939530fdfa09ee4
Israel <israel.carvalho@sou.inteli.edu.br>
2022-06-02 23:12:13 -0300

    integração BACK inicial

--- Essa(EGON)/backend/app_61.js
+++ Essa(EGON)/backend/app_61.js
@@ -85,7 +85,7 @@ app.get('/profissionais', (req, res) => {
 		}
 	res.write(`<tr> <td><a data-toggle="modal" href="#modalhoras"><div class="modal_professional" > ${nomes[i]} </div></a></td><td> ${campos[i]} </td><td> ${tipos[i]} </td><td> ${cargos[i]} </td><td> <span class="${quadrado}"> ${horas[i]}/120/176 </span> </td></tr>`);
 	};
-	res.write(`</tbody> </table> </div><div id="menu"> <div class="centro"> <a href="abageral.html" id="logo_tela_inicial"> <img id="logo" src="img/logo2.png" alt=""> </a> </div><nav id="nav-menu"> <ul class="centro"> <div class="ative-profi" > <li class="ordem"><span class="material-symbols-outlined"> person_search </span>Profissionais</li></div><div class="projeto"> <li class="ordem"><a class="centra ordem" href="abageral.html"><span class="material-symbols-outlined"> task</span>Projetos</a></li></div><div class="modal-style" data-toggle="modal" data-target="#modalpro" id="buttonmodal"> <li class="ordem"> <span class="material-symbols-outlined"> add </span>Adicionar</li></div></ul> </nav> </div></div><div class="modal fade" id="modalpro" role="dialog"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> </div><div class="modal-body"> <div id="nome"> <label for="nome">Nome do Funcionario</label> <input name="nome" type="text"> </div><div id="unidade"> <label for="unidades">Unidades: </label> <select name="unidades" id="unidades"> <option value="sp">SP</option> <option value="am">AM</option> <option value="ambos">Ambos</option> </select> </div><div id="inicio"> <label for="CLT">CLT?:</label> <select name="CLT" id="CLT"> <option value="simclt">Sim</option> <option value="naoclt">Nao</option> </select> </div><div id="horastrabalhadas"> <label for="horastrab">Horas Mensais: </label> <input type="number" name="horastrab"> <label for="horasproj">Horas Mensais para Projetos: </label> <input type="number" name="horasproj"> </div><div id="inicio"> <label for="areas">Área Trabalhada: </label> <select name="areas" id="areas"> <option value="area1">Área 1</option> <option value="area2">Área 2</option> <option value="area3">Área 3</option> <option value="area4">Área 4</option> <option value="area5">Área 5</option> </select> </div></div><div class="modal-footer"> <a class="hand_hover" data-dismiss="modal">Criar</a> </div></div></div></div></div><div class="modal fade" id="modalhoras" role="dialog"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> </div><div class="modal-body"> </div><div id="tabhor"> <label for="Janeiro">Janeiro: </label> <input id='Janeiro' type="number" name="Janeiro" step="1" value="0"> <br><label for="Fevereiro">Fevereiro: </label> <input id='Fevereiro' type="number" name="Fevereiro" step="1" value="0"> <br><label for="Março"> Março: </label> <input id='Março' type="number" name="Março" step="1" value="0"> <br><label for="Abril"> Abril: </label> <input id='Abril' type="number" name="Abril" step="1" value="0"> <br><label for="Maio"> Maio: </label> <input id='Maio' type="number" name="Maio" step="1" value="0"> <br><label for="Junho"> Junho: </label> <input id='Junho' type="number" name="Junho" step="1" value="0"> <br><label for="Julho"> Julho: </label> <input id='Julho' type="number" name="Julho" step="1" value="0"> <br><label for="Agosto"> Agosto: </label> <input id='Agosto' type="number" name="Agosto" step="1" value="0"> <br><label for="Setembro"> Setembro: </label> <input id='Setembro' type="number" name="Setembro" step="1" value="0"> <br><label for="Outubro"> Outubro: </label> <input id='Outubro' type="number" name="Outubro" step="1" value="0"> <br><label for="Novembro"> Novembro: </label> <input id='Novembro' type="number" name="Novembro" step="1" value="0"> <br><label for="Dezembro"> Dezembro: </label> <input id='Dezembro' type="number" name="Dezembro" step="1" value="0"> <br></div></div><div class="modal-footer"> <button class="hand_hover" type="button" data-dismiss="modal">Editar</button> </div></div></div></div></div></div></body></html>`);
+	res.write(`</tbody> </table> <form action="/profissionais/adicionar" method="post" ><input name="name" value="Insert Name"><input name="hours" value="Insert Hours"><input name="field" value="Insert Field"><input name="role" value="Insert Role"><input name="type" value="Insert Type"><button>ADICIONAR MEU ACESSO</button></form> </div><div id="menu"> <div class="centro"> <a href="abageral.html" id="logo_tela_inicial"> <img id="logo" src="img/logo2.png" alt=""> </a> </div><nav id="nav-menu"> <ul class="centro"> <div class="ative-profi" > <li class="ordem"><span class="material-symbols-outlined"> person_search </span>Profissionais</li></div><div class="projeto"> <li class="ordem"><a class="centra ordem" href="abageral.html"><span class="material-symbols-outlined"> task</span>Projetos</a></li></div><div class="modal-style" data-toggle="modal" data-target="#modalpro" id="buttonmodal"> <li class="ordem"> <span class="material-symbols-outlined"> add </span>Adicionar</li></div></ul> </nav> </div></div><div class="modal fade" id="modalpro" role="dialog"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> </div><div class="modal-body"> <div id="nome"> <label for="nome">Nome do Funcionario</label> <input name="nome" type="text"> </div><div id="unidade"> <label for="unidades">Unidades: </label> <select name="unidades" id="unidades"> <option value="sp">SP</option> <option value="am">AM</option> <option value="ambos">Ambos</option> </select> </div><div id="inicio"> <label for="CLT">CLT?:</label> <select name="CLT" id="CLT"> <option value="simclt">Sim</option> <option value="naoclt">Nao</option> </select> </div><div id="horastrabalhadas"> <label for="horastrab">Horas Mensais: </label> <input type="number" name="horastrab"> <label for="horasproj">Horas Mensais para Projetos: </label> <input type="number" name="horasproj"> </div><div id="inicio"> <label for="areas">Área Trabalhada: </label> <select name="areas" id="areas"> <option value="area1">Área 1</option> <option value="area2">Área 2</option> <option value="area3">Área 3</option> <option value="area4">Área 4</option> <option value="area5">Área 5</option> </select> </div></div><div class="modal-footer"> <a class="hand_hover" data-dismiss="modal">Criar</a> </div></div></div></div></div><div class="modal fade" id="modalhoras" role="dialog"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> </div><div class="modal-body"> </div><div id="tabhor"> <label for="Janeiro">Janeiro: </label> <input id='Janeiro' type="number" name="Janeiro" step="1" value="0"> <br><label for="Fevereiro">Fevereiro: </label> <input id='Fevereiro' type="number" name="Fevereiro" step="1" value="0"> <br><label for="Março"> Março: </label> <input id='Março' type="number" name="Março" step="1" value="0"> <br><label for="Abril"> Abril: </label> <input id='Abril' type="number" name="Abril" step="1" value="0"> <br><label for="Maio"> Maio: </label> <input id='Maio' type="number" name="Maio" step="1" value="0"> <br><label for="Junho"> Junho: </label> <input id='Junho' type="number" name="Junho" step="1" value="0"> <br><label for="Julho"> Julho: </label> <input id='Julho' type="number" name="Julho" step="1" value="0"> <br><label for="Agosto"> Agosto: </label> <input id='Agosto' type="number" name="Agosto" step="1" value="0"> <br><label for="Setembro"> Setembro: </label> <input id='Setembro' type="number" name="Setembro" step="1" value="0"> <br><label for="Outubro"> Outubro: </label> <input id='Outubro' type="number" name="Outubro" step="1" value="0"> <br><label for="Novembro"> Novembro: </label> <input id='Novembro' type="number" name="Novembro" step="1" value="0"> <br><label for="Dezembro"> Dezembro: </label> <input id='Dezembro' type="number" name="Dezembro" step="1" value="0"> <br></div></div><div class="modal-footer"> <button class="hand_hover" type="button" data-dismiss="modal">Editar</button> </div></div></div></div></div></div></body></html>`);
 	});
 	db.close(); // Fecha o banco
 });
@@ -95,7 +95,7 @@ app.post('/profissionais/adicionar', urlencodedParser, (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
-	sql = "INSERT INTO profissionais (name, hours, field, role, type) VALUES ('" + req.body.name + "', " + req.body.hours + ",'"+ req.body.field +"','" + req.body.role +"','" + req.body.type +"')";
+	sql = "INSERT INTO profissionais (name, hours, field, role, type) VALUES ('" + req.body.name + "', " + req.body.hours + ",'"+ req.body.field +"','" + req.body.role + "','" + req.body.type +"')";
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
 	console.log(sql);
 	db.run(sql, [],  err => {
@@ -131,7 +131,7 @@ app.delete('/profissionais/deletar', urlencodedParser, (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
-	sql = "DELETE FROM profissionais WHERE name = " + "'" + req.body.name + "'";
+	sql = "DELETE FROM profissionais WHERE id = " + "'" + req.body.id + "'";
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
 	db.run(sql, [],  err => {
 		if (err) {
@@ -142,11 +142,49 @@ app.delete('/profissionais/deletar', urlencodedParser, (req, res) => {
 	db.close(); // Fecha o banco
 });
 
+//////////////////////////////////PROJETOS/////////////////////////
+
+//exibe todos os projetos registrados
+app.get('/projetos', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = 'SELECT * FROM projetos ORDER BY name COLLATE NOCASE';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		else console.log(sql);
+
+	res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+//insere um novo projeto (C do CRUD)
+app.post('/projetos/adicionar', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Acess-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = `INSERT INTO projetos (name, area, numberFunc, duration) VALUES ('${req.body.name}' , '${req.body.area}' , ${req.body.numberFunc} , ${req.body.duration})`;
+	var db = new sqlite3.Database(DBPATH); //Abre o banco
+	console.log(sql);
+	db.run (sql, [], err => {
+		if (err) {
+			throw err;
+		}
+		else console.log(sql);
+	});
+	db.close(); //fecha o banco
+	res.end();
+});
+
 // Atualiza um registro (é o U do CRUD - Update)
 app.patch('/projetos/atualizar', urlencodedParser, (req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
-	sql = `UPDATE projetos SET name = '${req.body.name}', area = '${req.body.area}' , numberFunc = ${req.body.numberFunc} , duration = ${req.body.duration} WHERE id = ${req.body.id}`;
+	sql = `UPDATE projetos SET name = '${req.body.name}' , area = '${req.body.area}' , numberFunc = ${req.body.numberFunc} , duration = ${req.body.duration} WHERE id = ${req.body.id}`;
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
 	db.run(sql, [],  err => {
 		if (err) {
@@ -155,6 +193,22 @@ app.patch('/projetos/atualizar', urlencodedParser, (req, res) => {
 		res.end();
 	});
 	db.close(); // Fecha o banco
+});
+
+
+// Exclui um projeto
+app.delete('/projetos/deletar', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+    sql = `DELETE FROM projetos WHERE id = ${req.body.id}`;
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.end();
+    });
+    db.close(); // Fecha o banco
 });
 
 /* Inicia o servidor */
