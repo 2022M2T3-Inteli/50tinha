b1c4a4c97f9466daaccc72d6a18bd858863e018b
Israel <israel.carvalho@sou.inteli.edu.br>
2022-06-09 11:43:41 -0300

    Tabela profissionais ajax

--- Essa(EGON)/frontend/script.js
+++ Essa(EGON)/frontend/script.js
@@ -30,3 +30,38 @@ function criarProjeto(){
     console.log(tamanho)    
     $("#c-todos > li:nth-child("+n+")").css("margin-left",mesInicio*82+"px")
 }
+
+//Requição AJAX
+const rodape = document.getElementById("rodape")
+rodape.onload = geraTabela()
+const tabela= document.getElementById("hover_tabela")
+
+function geraTabela(){
+    let request = new XMLHttpRequest();
+
+    request.onreadystatechange = function(){
+        let MyJson = JSON.parse(this.responseText);
+        let MyJsonSize = MyJson.length;
+        // tabela.innerHTML = ""
+        for(let i = 0; i < MyJsonSize; i++ ){
+            if(MyJson[i].hours < 50 ){
+                quadrado = "quadrado_verde";
+            }
+            else if(MyJson[i].hours >= 50 && MyJson[i].hours < 70){
+                quadrado = "quadrado_amarelo";
+            }
+            else if(MyJson[i].hours >= 70 && MyJson[i].hours < 88 ){
+                quadrado = "quadrado_vermelho";
+            }
+            else if(MyJson[i].hours >= 88 ){
+                quadrado = "quadrado_preto";
+            } 
+        tabela.innerHTML += `<tr> <td><a data-toggle="modal" href="#modalhoras"><div class="modal_professional" > ${MyJson[i].name} </div></a></td> <td> ${MyJson[i].role} </td><td> ${MyJson[i].type} </td><td> ${MyJson[i].field} </td>  <td> <span class=${quadrado}> ${MyJson[i].hours}/170/176 </span> </td></tr>`;
+        }
+    }
+
+    let url = "http://localhost:3061/profissionais"
+    request.open("GET", url, true);
+    request.send()
+    
+}
