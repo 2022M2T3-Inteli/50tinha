4d4e951437ef2812a6adb2ed7774ba3657163a1f
joão Vitor oliveira Rodrigues <joao.rodrigues@sou.inteli.edu.br>
2022-06-22 00:12:11 -0300

    EditarProjeto

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -17,6 +17,8 @@ function frg(){
     }
 }
 
+var nomes = new Map();
+
 var x = 0
 
 
@@ -81,19 +83,22 @@ function geraTabelaProj(){
         <th>Duração</th>
         <th>Nº Funcionários/Projetos</th>
         <th>Unidade</th>
-        <th>Ações</th>
+        <th>Ano</th>
+        <th class="acoesProjeto">Ações</th>
         </tr>`
         for(let i = 0; i < tamanhoDados; i++){
-            let noSpaceName = dados[i].nome.replace(" ", "_")
             duracao.push((dados[i].anoFim - dados[i].anoInicio)*12 + eval("meses." + dados[i].mesFim) - eval("meses."+ dados[i].mesInicio)) // O if estava sendo inútil, então tirei o código dele
-            console.log(`${dados[i].idProject},${dados[i].nome},${dados[i].mesInicio},${dados[i].mesFim}`)
-            tabelaProj.innerHTML +=`<tr id="projeto_${dados[i].idProject}"> <td id="coldata" class="aba"> <a href="#modalgraphs" data-toggle="modal">${dados[i].nome}</a> </td><td id="coldata" class="aba"> <a href="#modalgraphs" data-toggle="modal">${duracao[i]} Meses</a> <center> </td><td id="coldata"> <a href="#modalgraphs" data-toggle="modal">${dados[i].numberFunc}</a> <center> <td>${dados[i].unidade}</td><td><div class="linha"> <a href="#" onclick="editar(${dados[i].idProject},${noSpaceName},${dados[i].mesInicio},${dados[i].mesFim});" >Editar</a> <a href="/projetos.html" onclick="excluirProjeto(${dados[i].idProject})">Excluir</a> </div></td></tr>`
-        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+            nomes.set("nP"+dados[i].idProject,[dados[i].nome,dados[i].unidade])
+            tabelaProj.innerHTML +="<tr id=\"projeto_"+dados[i].idProject+"\"> <td id=\"coldata\" class=\"aba\"> <a href=\"#modalgraphs\" data-toggle=\"modal\">"+dados[i].nome+"</a> </td><td id=\"coldata\" class=\"aba\"> <a href=\"#modalgraphs\" data-toggle=\"modal\"> "+duracao[i]+"Meses</a> <center> </td><td id=\"coldata\"> <a href=\"#modalgraphs\" data-toggle=\"modal\">"+dados[i].numberFunc+"</a> <center> <td>"+dados[i].unidade+"</td><td>"+dados[i].anoInicio+" a "+dados[i].anoFim+"</td><td><div class=\"linha\"> <a href=\"#\" onclick=\"editar("+dados[i].idProject+","+eval("meses."+ dados[i].mesInicio)+","+eval("meses." + dados[i].mesFim)+","+dados[i].anoInicio+","+dados[i].anoFim+");\" >Editar</a> <a href=\"/projetos.html\" onclick=\"excluirProjeto("+ dados[i].idProject+")\">Excluir</a> </div></td></tr>"
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
     }
     url = "/projetos"
     requestProj.open("GET", url, true)
     requestProj.send()
 }
+
+
+
 
 //POST PROJETOS
 function enviaProjeto(){
@@ -125,7 +130,7 @@ function enviaProjeto(){
     });
 }
 
-var meses = {
+var meses = { // Ajuda nas conversões necessarias de number para string
     janeiro: 0,
     fevereiro: 1,
     março: 2,
@@ -137,9 +142,23 @@ var meses = {
     setembro: 8,
     outubro: 9,
     novembro: 10,
-    dezembro: 11
+    dezembro: 11,
+    n0: "janeiro",
+    n1: "fevereiro",
+    n2: "março",
+    n3: "abril",
+    n4: "maio",
+    n5: "junho",
+    n6: "julho",
+    n7: "agosto",
+    n8: "setembro",
+    n9: "outubro",
+    n10: "novembro",
+    n11: ""
 }
 
+
+// Coleta os profissionais do banco de dados e passa suas informações como argumento para função alocacao(),  Linha:167
 function getEmployees(){
     let requestLines = new XMLHttpRequest();
     requestLines.onload = function(){
@@ -156,6 +175,7 @@ function getEmployees(){
     requestLines.send();
 }
 
+// Exclui os projetos
 function excluirProjeto(idP){
     url = "/projetos/deletar"
     $.ajax({
@@ -171,14 +191,58 @@ function excluirProjeto(idP){
     });
 }
 
-function editar(idP,nomeP,iniP,fimP){
-    let spaceName = nomeP.replace("_", " ")
-    console.log(spaceName)
-
-    document.getElementById(index).innerHTML = "<div>olá</div>"
+// Edita os projetos
+function editar(idP,inicio,fim,anoI,anoF){
+    let anoFim = anoF
+    let anoInicio = anoI
+    let idProjeto = "projeto_"+idP
+    let mesInicio = inicio.toString()
+    let mesFim = fim.toString()
+    let lista = nomes.get("nP"+idP) // indice [0] reprensta o nome do projeto; indice [1] reprensta a cidade do projeto 
+    let selecao =""
+    // let duracao = (parseInt(anoFim) - parseInt(anoInicio)*12 + parseInt(mesFim) - parseInt(mesInicio) )
+    mesInicio = eval("meses.n"+mesInicio)
+    mesFim = eval("meses.n"+mesFim)
+    if(lista[1] == "AM"){
+        selecao = "<select><option value=\"am\">AM</option><option value=\"sp\">SP</option><option value=\"ambos\">Ambos</option> </select>"
+    }else{selecao = "<select><option value=\"sp\">SP</option><option value=\"am\">AM</option><option value=\"ambos\">Ambos</option> </select>"}
+    document.getElementById(idProjeto).innerHTML = "<form method=\"post\"><td id=\"coldata\" class=\"aba\"><input  type=\"text\" placeholder=\""+lista[0]+"\"></td>  <td id=\"coldata\" class=\"aba\"><input  type=\"text\" placeholder=\""+mesInicio+"\"><input  type=\"text\" placeholder=\""+mesFim+"\"></td>  <td id=\"coldata\">Auto</td> <td>"+selecao+"</td> <td><input  type=\"text\" placeholder=\""+anoInicio+"\"> a <input  type=\"text\" placeholder=\""+anoFim+"\"></td> <td><div class=\"linha\"> <button class=\"hand_hover\" type=\"submit\" onclick=\"atualizar("+idP+");\" >Confirmar</button> <a href=\"/projetos.html\" onclick=\"excluirProjeto()\">Excluir</a> </div></td></form>"
+    
+    // Impossibilita que os de mais projetos sejam clicaveis
+        // tabelaProj.innerHTML +="<tr id=\"projeto_"+dados[i].idProject+"\"> <td id=\"coldata\" class=\"aba\"> <a href=\"#modalgraphs\" data-toggle=\"modal\">"+dados[i].nome+"</a> </td><td id=\"coldata\" class=\"aba\"> <a href=\"#modalgraphs\" data-toggle=\"modal\"> "+duracao+"Meses</a> <center> </td><td id=\"coldata\"> <a href=\"#modalgraphs\" data-toggle=\"modal\">"+dados[i].numberFunc+"</a> <center> <td>"+dados[i].unidade+"</td><td>"+dados[i].anoInicio+" a "+dados[i].anoFim+"</td><td><div class=\"linha\"> <a href=\"#\" onclick=\"editar("+dados[i].idProject+","+eval("meses."+ dados[i].mesInicio)+","+eval("meses." + dados[i].mesFim)+","+dados[i].anoInicio+","+dados[i].anoFim+");\" >Editar</a> <a href=\"/projetos.html\" onclick=\"excluirProjeto("+ dados[i].idProject+")\">Excluir</a> </div></td></tr>"
 
 }
 
+// Atualiza os projetos
+function atualizar(idP){
+    const nomeP = document.getElementById('nomeFunc').value;
+    const mesInicio = document.getElementById('unidades').value;
+    const anoInicio = document.getElementById('CLT').value;
+    const mesFim = document.getElementById('areas').value;
+    const anoFim = document.getElementById('areas').value;
+
+    url = "/projetos/deletar"
+    $.ajax({
+        type: "PATCH",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: JSON.stringify(
+            {   
+                "nome": nomeP,
+                "area": "Teste",
+                "mesInicio": mesInicio,
+                "anoInicio": anoInicio,
+                "mesFim": mesFim,
+                "anoFim": anoFim,
+                "idProject": idP,   
+            }
+        )
+    });
+    window.location.reload(); //essa função mágica faz com que atualize a página, sem haver a necessidade de recarregar manualmente para ver a adição do profissional na tabela
+}
+
+// Adiciona os profissionais do banco de dados ao modal de alocação
 function alocacao(nomedb,iddb){
     var nome = nomedb
     var id = iddb
