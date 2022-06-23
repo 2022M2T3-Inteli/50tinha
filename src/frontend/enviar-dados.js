7fd16d828667c49e06d860e5d71d2dc159df06eb
Pablo-RLV <pablo.viana@sou.inteli.edu.br>
2022-06-23 01:11:09 -0300

    merge israel e joão

--- src/frontend/enviar-dados.js
+++ src/frontend/enviar-dados.js
@@ -21,3 +21,71 @@ function enviarDados(){  ///////////////////COLOCAR O ID DO PROFISSIONAL, PQ EST
     });
     window.location.reload(); //essa função mágica faz com que atualize a página, sem haver a necessidade de recarregar manualmente para ver a adição do profissional na tabela
 }
+
+const assignBtn = document.getElementById("assign-btn");
+assignBtn.onclick = () => assignProfIntoProj()
+
+var idClickedProj
+$(document).ready(function(){
+    $('body').on('click', '.aAssign', function (e) {
+        idClickedProj = $(this).closest('tr').attr('id')
+        let finalId = ""
+        for(i = 8; i < idClickedProj.length; i++){
+            finalId += idClickedProj.charAt(i)
+        }
+        idClickedProj = finalId
+    })
+})
+
+function assignProfIntoProj(){
+    const idProf = Number(document.getElementById("alocacaos").value)
+    console.log(idProf)
+    const horasAlocadas = Number(document.getElementById("horasAlocadas").value)
+    url1 = "/alocacao/acidionar/profissional"
+    url2 = "/projetos/single/"
+    url3 = "/alocacao/adicionar"
+    Number(idClickedProj)
+    let params = JSON.stringify({idProject: Number(idClickedProj)});
+    console.log(params)
+    let clickedProj = new XMLHttpRequest();
+    clickedProj.onload = function(){
+        let data = JSON.parse(this.responseText)
+        let dataLength = data.length
+        console.log(data[0].nome + " pegou krll")
+        let idFunc = idProf
+        let idProj = data[0].idProject
+        let horasAlocadasProj = horasAlocadas
+        let mesProj = data[0].mesInicio
+        let anoProj = data[0].anoInicio
+        //Adiciona uma nova alocação
+        $.ajax({
+            type: "POST",
+            url: url3,
+            contentType: "application/json; charset=utf-8",  //por padrão, temos que avisar que a aplicação é do tipo json e que os caracteres aceitam caracteres especiais
+            dataType: "json", //o conteúdo do dado é json
+            data: JSON.stringify(  //transforma os valores em uma string do tipo json
+                {
+                    "idFunc": idFunc, //idFunc, idProject, horasAlocadasProjeto, mes, ano
+                    "idProject": idProj,
+                    "horasAlocadasProjeto": horasAlocadasProj,
+                    "mes": mesProj,
+                    "ano": anoProj
+                }   
+            )
+        });
+
+        // $.ajax({
+        //     type: "PATCH",
+        //     url: url1,
+        //     contentType: "application/json; charset=utf-8",  //por padrão, temos que avisar que a aplicação é do tipo json e que os caracteres aceitam caracteres especiais
+        //     dataType: "json", //o conteúdo do dado é json
+        //     data: JSON.stringify(  //transforma os valores em uma string do tipo json
+        //         {
+        //             "idAlocacao": 
+        //         }   
+        //     )
+        // });
+    }
+    clickedProj.open("GET", url2+idClickedProj, true)
+    clickedProj.send(params)
+}
